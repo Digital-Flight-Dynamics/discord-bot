b3b55b6449553c7c5c181d4cfba1fe028b864f29
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2021-12-01 20:37:21 -0600

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+    console.log('Bot is logged in!');
+});
+
+client.login(token);
