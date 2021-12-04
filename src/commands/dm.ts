a1c5fe37e68352c56a6e5ae2d4da0c7315c0ca38
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2021-12-03 20:19:53 -0600

    feat: Add .dm command

---
+++ src/commands/dm.ts
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'dm',
+    async execute(message, args, config, client, Discord) {
+        if (!message.member.permissions.has('MANAGE_MESSAGES')) return;
+
+        const user = message.mentions.users.first();
+
+        const content = args.slice(1).join(' ');
+
+        const dmEmbed = new Discord.MessageEmbed().setColor(config.embedColor).setTitle('Digital Flight Dynamics').setDescription(content);
+        await user.createDM().then((dm) => dm.send({ embeds: [dmEmbed] }));
+
+        const embed = new Discord.MessageEmbed()
+            .setColor(config.embedColor)
+            .setTitle('DM User')
+            .setDescription(`DM sent to ${user}`)
+            .addFields({ name: 'Content', value: `${content}` });
+        message.channel.send({ embeds: [embed] });
+    },
+};
