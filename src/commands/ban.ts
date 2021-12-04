c9f298acc6be50acd769347086a3e8945c916202
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2021-12-04 00:33:27 -0600

    feat: Add .ban command

---
+++ src/commands/ban.ts
@@ -0,0 +1,44 @@
+module.exports = {
+    name: 'ban',
+    async execute(message, args, config, client, Discord) {
+        if (!message.member.permissions.has('BAN_MEMBERS')) return;
+
+        const user = message.mentions.users.first();
+        const member = message.guild.members.cache.get(user.id);
+
+        if (!user) {
+            await message.channel.send({
+                embeds: [
+                    new Discord.MessageEmbed()
+                        .setColor(config.embedColor)
+                        .setTitle('Error')
+                        .setDescription('Please mention a valid user'),
+                ],
+            });
+            return;
+        }
+
+        const reason = args.slice(1).join(' ') || 'None';
+
+        const dmEmbed = new Discord.MessageEmbed()
+            .setColor(config.embedColor)
+            .setTitle(`Banned from ${message.guild.name}`)
+            .addFields(
+                { name: 'Reason', value: `${reason}`, inline: true },
+                { name: 'Moderator', value: `${message.author.tag}`, inline: true },
+            );
+        await user.send({ embeds: [dmEmbed] });
+
+        await member.ban({ reason: [reason] });
+
+        const embed = new Discord.MessageEmbed()
+            .setColor(config.embedColor)
+            .setTitle('Ban User')
+            .setDescription(`${user.tag} has been banned.`)
+            .addFields(
+                { name: 'Reason', value: `${reason}`, inline: true },
+                { name: 'Moderator', value: `${message.author.tag}`, inline: true },
+            );
+        await message.channel.send({ embeds: [embed] });
+    },
+};
