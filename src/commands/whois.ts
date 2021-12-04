bf86c41e58451c9d3f514d7c6dcc8f39a5b816c4
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2021-12-03 18:33:09 -0600

    fix .whois command getting wrong server member

--- src/commands/whois.ts
+++ src/commands/whois.ts
@@ -2,7 +2,8 @@ module.exports = {
     name: 'whois',
     execute(message, args, config, client, Discord) {
         const user = message.mentions.users.first();
-        const joined = message.member.joinedAt.toString().split(' ');
+        const member = message.guild.members.cache.get(user.id);
+        const joined = member.joinedAt.toString().split(' ');
         const registered = user.createdAt.toString().split(' ');
 
         const embed = new Discord.MessageEmbed()
@@ -13,9 +14,9 @@ module.exports = {
             .addFields(
                 { name: 'Registered', value: `${registered[0]}, ${registered[1]} ${registered[2]}, ${registered[3]} at ${registered[4]} GMT`, inline: true },
                 { name: 'Joined', value: `${joined[0]}, ${joined[1]} ${joined[2]}, ${joined[3]} at ${joined[4]} GMT`, inline: true },
-                { name: 'Nickname', value: message.member.nickname === null ? `None` : `${message.member.nickname}` },
-                { name: 'Role Count', value: `${message.member.roles.cache.size - 1}`, inline: true },
-                { name: `Highest Role`, value: `${message.member.roles.highest}`, inline: true },
+                { name: 'Nickname', value: member.nickname === null ? `None` : `${member.nickname}` },
+                { name: 'Role Count', value: `${member.roles.cache.size - 1}`, inline: true },
+                { name: `Highest Role`, value: `${member.roles.highest}`, inline: true },
             )
             .setFooter(`ID: ${user.id}`);
 
