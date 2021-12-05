bf89e3385548f27835d139889184d1fc0cf458bc
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2021-12-05 11:19:10 -0600

    feat: Add .membercount

--- src/commands/general/membercount.ts
+++ src/commands/general/membercount.ts
@@ -2,7 +2,21 @@ module.exports = {
     name: 'membercount',
     async execute(message, args, config, client, Discord) {
         const count = message.guild.memberCount;
-        const embed = new Discord.MessageEmbed().setColor(config.embedColor).setTItle('Members').setDescription(`${count}`);
+        const areBots = message.guild.members.cache.map((member) => member.user.bot);
+
+        let bots = 0;
+        for (const b of areBots) {
+            if (b) bots++;
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setColor(config.embedColor)
+            .setTitle('Member Count')
+            .addFields(
+                { name: 'Total Members', value: `${count}` },
+                { name: 'Users', value: `${count - bots}`, inline: true },
+                { name: 'Bots', value: `${bots}`, inline: true },
+            );
 
         await message.channel.send({ embeds: [embed] });
     },
