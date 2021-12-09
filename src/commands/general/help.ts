6ae1456f5d12de57971386a0f3c5464b761dddeb
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2021-12-08 21:45:46 -0600

    fix: add examples to .help command

--- src/commands/general/help.ts
+++ src/commands/general/help.ts
@@ -20,28 +20,34 @@ module.exports = {
 
         category = category.toLowerCase();
 
-        const embed = new Discord.MessageEmbed().setColor(config.embedColor).setTitle(`${category}`);
+        const embed = new Discord.MessageEmbed().setColor(config.embedColor);
 
         if (category === 'a35x') {
-            embed.addFields(
-                { name: '.marketplace', value: 'Will the A35X come to the MSFS Marketplace or Xbox?' },
-                { name: '.when', value: 'When is the A35X coming?' },
-            );
+            embed
+                .setTitle('A35X')
+                .addFields(
+                    { name: '.marketplace', value: 'Will the A35X come to the MSFS Marketplace or Xbox?' },
+                    { name: '.when', value: 'When is the A35X coming?' },
+                );
         } else if (category === 'general') {
-            embed.addFields(
-                { name: '.membercount', value: 'Displays the amount of members in the server' },
-                { name: '.metar', value: 'Shows the metar for the selected airport (first argument). e.g. `.metar EGLL`' },
-            );
+            embed
+                .setTitle('General')
+                .addFields(
+                    { name: '.membercount', value: 'Displays the amount of members in the server' },
+                    { name: '.metar', value: 'Shows the metar for the selected airport (`.metar ICAO`)' },
+                );
         } else if (category === 'moderation') {
-            embed.addFields(
-                { name: '.ban', value: 'Bans the mentioned user' },
-                { name: '.dm', value: 'Sends a DM to the mentioned user' },
-                { name: '.kick', value: 'Kicks the mentioned user' },
-                { name: '.purge', value: 'Clears the specified amount of messages' },
-                { name: '.whois', value: 'Gives information about the mentioned user' },
-            );
+            embed
+                .setTitle('Moderation')
+                .addFields(
+                    { name: '.ban', value: 'Bans the mentioned user (`.ban <@id> ban reason here`)' },
+                    { name: '.dm', value: 'Sends a DM to the mentioned user (`.dm <@id> dm message here`)' },
+                    { name: '.kick', value: 'Kicks the mentioned user (`.kick <@id> kick reason here`)' },
+                    { name: '.purge', value: 'Clears the specified amount of messages (`.purge amount`)' },
+                    { name: '.whois', value: 'Gives information about the mentioned user (`.whois <@id>`)' },
+                );
         } else if (category === 'fun') {
-            embed.addFields({ name: '.fun', value: 'A placeholder fun command' });
+            embed.setTitle('Fun').addFields({ name: '.fun', value: 'A placeholder fun command' });
         } else {
             return;
         }
