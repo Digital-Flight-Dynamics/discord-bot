1693676b8d26347559b00da38c68b0b9ac9a450e
Cleon <cleonthegamer@gmail.com>
2022-01-03 01:18:24 +0400

    feat: add .msfs command

---
+++ src/commands/general/msfs.ts
@@ -0,0 +1,20 @@
+import Discord from 'discord.js';
+import { CommandCategories, CommandDefinition } from '../index';
+import { color } from '../../index';
+
+export const msfs: CommandDefinition = {
+    names: ['msfs'],
+    description: "Redirects user to MSFS server for support",
+    category: CommandCategories.GENERAL,
+    execute: async (message, args) => {
+        const embed = new Discord.MessageEmbed()
+            .setColor(color)
+            .setTitle('Sim Issue')
+            .setDescription(
+                'This is an issue regarding Microsoft Flight Simulator, as this is the Digital Flight Dynamics A350X Project server, we are unable to provide support for the same.'+
+                'For core simulator issues, we recommend you visit [Microsoft Flight Simulator\'s Discord](https://discord.gg/msfs) or the official [Microsoft Flight Simulator Forum](https://forums.flightsimulator.com/c/community/140).'
+            );
+
+        await message.channel.send({ embeds: [embed] }).catch(console.error);
+    },
+};
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -14,6 +14,7 @@ import { unban } from './moderation/unban';
 import { kick } from './moderation/kick';
 import { dm } from './moderation/dm';
 import { whois } from './moderation/whois';
+import { msfs } from './general/msfs'
 
 export const enum CommandCategories {
     A350X = 'A350X',
@@ -30,4 +31,4 @@ export type CommandDefinition = {
 };
 export const createErrorEmbed = (description: string) => new Discord.MessageEmbed().setColor('#FF0000').setTitle('Error').setDescription(description);
 
-export const commands: CommandDefinition[] = [when, marketplace, faq, help, whened, whoosh, info, rules, reactionroles, purge, ban, unban, kick, dm, whois];
+export const commands: CommandDefinition[] = [when, marketplace, faq, help, whened, whoosh, info, rules, reactionroles, purge, ban, unban, kick, dm, whois, msfs];
