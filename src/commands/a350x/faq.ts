057574bb7f8b33b02637784b41a38ba20b3085ae
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2022-01-02 15:05:52 -0600

    fix: A35X -> A350X in .faq

--- src/commands/a350x/faq.ts
+++ src/commands/a350x/faq.ts
@@ -14,19 +14,19 @@ export const faq: CommandDefinition = {
             .setDescription('This will hopefully answer most of your questions. If you have any other questions, you may ask in <#808791475206094928>')
             .addFields(
                 {
-                    name: 'Q: When will the A35X release?',
+                    name: 'Q: When will the A350X release?',
                     value: 'A: It will release as an alpha version when all visuals are complete and when systems/avionics are usable for normal operations.',
                 },
                 {
                     name: 'Q: Freeware or payware?',
-                    value: 'A: The A35X addon will be completely freeware AND open-source!',
+                    value: 'A: The A350X addon will be completely freeware AND open-source!',
                 },
                 {
                     name: 'Q: Will it have a custom cockpit?',
                     value: 'A: Yes, this addon will have a highly accurate custom cockpit.',
                 },
                 {
-                    name: 'Q: I want to help development of the project, so where can I start?',
+                    name: 'Q: I want to help development of the project. Where can I start?',
                     value:
                         'A: You can apply to join the development team using [this form](https://forms.gle/LigLwWizG5Etz3KeA). ' +
                         "Once you've completed the application, our team will have a look at it!",
