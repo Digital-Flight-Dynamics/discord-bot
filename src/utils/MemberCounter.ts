903fbbc70039a03d596627bb1bb59d3ff896c95a
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2022-01-02 15:05:23 -0600

    Restructure utils and prevent crashes by logging errors

--- src/utils/MemberCounter.ts
+++ src/utils/MemberCounter.ts
@@ -1,9 +1,23 @@
-export const MemberCounter = async (client) => {
-    const guild = await client.guilds.fetch('808790838163406848').catch(console.error);
-    setInterval(() => {
-        const { memberCount } = guild;
-        const channel = guild.channels.cache.find((c) => c.name.includes('Member Count:'));
+export const memberCounter = {
+    event: 'ready',
+    execute: (client) => {
+        const guild = client.guilds.cache.first();
 
-        channel.setName(`Member Count: ${memberCount.toLocaleString()}`);
-    }, 5000);
+        if (!guild) {
+            console.error('Error: Bot is not in a guild');
+            return;
+        }
+
+        setInterval(async () => {
+            const { memberCount } = guild;
+            const channel = guild.channels.cache.find((c) => c.name.includes('Member Count:'));
+
+            if (!channel) {
+                console.error('Error: Could not find member count channel');
+                return;
+            }
+
+            await channel.setName(`Member Count: ${memberCount.toLocaleString()}`);
+        }, 5000);
+    },
 };
