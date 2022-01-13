c0f9d01c77bd15b5b72cb1d5f262dfab74c11b0d
Fawwaz <72813433+Dynamic6448@users.noreply.github.com>
2022-01-12 18:39:16 -0600

    fix: can only use .whened after using .when (#7)
    Members can now only use .whened after .when is used. This will prevent any spam that may occur with .whened

--- src/commands/a350x/when.ts
+++ src/commands/a350x/when.ts
@@ -2,6 +2,8 @@ import Discord from 'discord.js';
 import { CommandCategories, CommandDefinition } from '../index';
 import { color } from '../../index';
 
+export let whenedAvailable;
+
 export const when: CommandDefinition = {
     names: ['when'],
     description: "Explains how we don't know when the A350X will release",
@@ -18,5 +20,11 @@ export const when: CommandDefinition = {
             );
 
         await message.channel.send({ embeds: [embed] }).catch(console.error);
+
+        setWhenedAvailable(true);
     },
+};
+
+export const setWhenedAvailable = (avail: boolean) => {
+    whenedAvailable = avail;
 };
