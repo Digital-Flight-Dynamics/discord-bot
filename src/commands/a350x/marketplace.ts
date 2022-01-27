4150e5b4e5e8cf90a5b2094a31ecbdbc61d19feb
Cleon <cleonthegamer@gmail.com>
2022-01-27 21:01:59 +0400

    Update marketplace.ts

--- src/commands/a350x/marketplace.ts
+++ src/commands/a350x/marketplace.ts
@@ -11,7 +11,7 @@ export const marketplace: CommandDefinition = {
             .setColor(color)
             .setTitle('Will the A350X come to the MSFS Marketplace or Xbox?')
             .setDescription(
-                'Microsoft does not allow Marketplace/Xbox addons to have copyleft licenses due to legal reasons.' +
+                    'Microsoft does not allow Marketplace/Xbox addons to have copyleft licenses due to legal reasons.' +
                     'Being committed freeware open sourced developers, we wish to remain under the GNU GPL license for our aircraft.' +
                     'However, it is also not possible to maintain 2 seperate versions for legal and practicability reasons.' +
                     'Furthermore, our addon could potentially run certain applications outside of the simulator, which would not be possible within the scope of the marketplace.',
