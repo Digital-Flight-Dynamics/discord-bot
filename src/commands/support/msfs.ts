56cde54f28209e8198279a95561e9e3840a2d78d
Cleon <cleonthegamer@gmail.com>
2022-01-27 21:12:33 +0400

    Update msfs.ts

--- src/commands/support/msfs.ts
+++ src/commands/support/msfs.ts
@@ -11,9 +11,9 @@ export const msfs: CommandDefinition = {
             .setColor(color)
             .setTitle('Sim Issue')
             .setDescription(
-                'This is an issue regarding Microsoft Flight Simulator. ' +
-                'As this is the Digital Flight Dynamics A350X project server, we are unable to provide support for the same. ' +
-                'For core simulator issues, we recommend you visit [Microsoft Flight Simulator\'s Discord](https://discord.gg/msfs) or the official [Microsoft Flight Simulator Forum](https://forums.flightsimulator.com/c/community/140).'
+                'The issue you have described is something regarding Microsoft Flight Simulator. ' +
+                'As this is the Digital Flight Dynamics A350X Project server, We are unable to provide support for the same. ' +
+                'For such core simulator issues, we recommend you visit [Microsoft Flight Simulator\'s Discord](https://discord.gg/msfs) or the official [Microsoft Flight Simulator Forum](https://forums.flightsimulator.com/c/community/140).'
             );
 
         await message.channel.send({ embeds: [embed] }).catch(console.error);
