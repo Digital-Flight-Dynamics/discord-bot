c63b00945e01f1d4c07791efa412b0d6e5dbbb77
Cleon <81078099+xZreai@users.noreply.github.com>
2022-01-28 08:09:24 +0400

    chore: update marketplace command (#9)

--- src/commands/a350x/marketplace.ts
+++ src/commands/a350x/marketplace.ts
@@ -11,10 +11,10 @@ export const marketplace: CommandDefinition = {
             .setColor(color)
             .setTitle('Will the A350X come to the MSFS Marketplace or Xbox?')
             .setDescription(
-                'Microsoft requires marketplace/Xbox planes to be licensed. ' +
-                    'This is not possible for freeware developers such as us due to the very high cost to obtain a license. ' +
-                    'Large aircraft manufacturers, such as Airbus and Boeing, would typically ask for an amount somewhere in the tens of thousands, even if the project is non-profit. ' +
-                    'Furthermore, Microsoft does not allow addons on the marketplace that use copyleft licenses such as GPL due to legal reasons.',
+                    'Microsoft does not allow Marketplace/Xbox addons to have copyleft licenses due to legal reasons. ' +
+                    'Being committed freeware open sourced developers, we wish to remain under the GNU GPL license for our aircraft. ' +
+                    'However, it is also not possible to maintain 2 seperate versions for legal and practicability reasons. ' +
+                    'Furthermore, our addon could potentially run certain applications outside of the simulator, which would not be possible within the scope of the marketplace. ',
             )
             .setFooter('TL;DR: It will neither come to the marketplace, nor will it come to Xbox.');
         await message.channel.send({ embeds: [embed] }).catch(console.error);
