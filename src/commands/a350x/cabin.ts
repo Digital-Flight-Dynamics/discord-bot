8c2b2454dd8d948bf0ce5b3c0298fd772286e0b8
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2022-08-04 18:36:21 -0500

    Update cabin.ts
    - formatting
    - rewrite embed title & description

--- src/commands/a350x/cabin.ts
+++ src/commands/a350x/cabin.ts
@@ -2,23 +2,19 @@ import Discord from 'discord.js';
 import { CommandCategories, CommandDefinition } from '../index';
 import { color } from '../../index';
 
-
 export const cabin: CommandDefinition = {
     names: ['cabin'],
-    description: "Explains the development plan on the A350X",
+    description: 'Informs others about our plans for the A350X cabin',
     category: CommandCategories.A350X,
     execute: async (message, args) => {
         const embed = new Discord.MessageEmbed()
             .setColor(color)
-            .setTitle('Does the project have a detailed cabin?')
+            .setTitle('Will there be a cabin?')
             .setDescription(
-                'Our team does their best to provide a high-quality and fucntioning cockpit. ' +
-                    'This means a functioning EFB and other real systems ' +
-                    'Feel free to leave a suggestion in <#808791599517663252>.',
+                'Yes, of course! There will be a detailed passenger cabin modeled for the A350X. ' +
+                    'However, as of now, we only plan on developing a generic cabin 3D model. This is due to the varied cabin layouts that airlines have.',
             );
 
         await message.channel.send({ embeds: [embed] }).catch(console.error);
-
     },
 };
-
