feba51cefc4d72a7884b7597f38a06020186b354
Fawwaz <72813433+Dynamic6448@users.noreply.github.com>
2022-10-04 17:52:42 -0500

    add simbrief command (#21)
    Co-Authored-By: Dynamic6448 <memonfaw@studentmail.conroeisd.net>

---
+++ src/commands/a350x/simbrief.ts
@@ -0,0 +1,17 @@
+import Discord from 'discord.js';
+import { CommandCategories, CommandDefinition } from '../index';
+import { color } from '../../index';
+
+export const simbrief: CommandDefinition = {
+    names: ['simbrief'],
+    description: 'Provides details about the simbrief integration',
+    category: CommandCategories.A350X,
+    execute: async (message, args) => {
+        const embed = new Discord.MessageEmbed()
+            .setColor(color)
+            .setTitle('Will there be a simbrief integration?')
+            .setDescription('Yes! Simbrief will be available to use via the OIS (Onboard Information System).');
+
+        await message.channel.send({ embeds: [embed] }).catch(console.error);
+    },
+};
