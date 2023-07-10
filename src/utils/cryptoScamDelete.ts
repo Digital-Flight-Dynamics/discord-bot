9d993ee20f5b0ae8442740957b20a8aa89d4393a
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2023-07-10 13:07:43 -0500

    improve filtering

--- src/utils/cryptoScamDelete.ts
+++ src/utils/cryptoScamDelete.ts
@@ -5,9 +5,19 @@ export const cryptoScamDelete: UtilDefinition = {
     event: 'messageCreate',
     execute: async (message: Discord.Message) => {
         if (message.channel.type === Discord.ChannelType.DM) return;
-
         const content = message.content.toLowerCase();
-        if (content.includes('earn') && content.includes('crypto market')) {
+
+        // basic filter
+        if (!content.includes('earn') || !content.includes('crypto market')) {
+            return;
+        }
+
+        if (
+            content.includes('https://') ||
+            message.content.includes('WhatsApp') ||
+            message.content.includes('Telegram') ||
+            content.includes('commission')
+        ) {
             await message.delete().catch(console.error);
         }
     },
