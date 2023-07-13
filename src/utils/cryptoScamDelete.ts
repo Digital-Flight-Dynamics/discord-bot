5051225da9de01bed67d207760c0c3f44d8c3764
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2023-07-12 21:29:30 -0500

    feat: image-text transcription for crypto scams

--- src/utils/cryptoScamDelete.ts
+++ src/utils/cryptoScamDelete.ts
@@ -1,22 +1,43 @@
 import Discord from 'discord.js';
+import { createWorker } from 'tesseract.js';
 import { UtilDefinition } from '.';
 
 export const cryptoScamDelete: UtilDefinition = {
     event: 'messageCreate',
     execute: async (message: Discord.Message) => {
         if (message.channel.type === Discord.ChannelType.DM) return;
-        const content = message.content.toLowerCase();
+
+        let content = message.content;
+        const image = message.attachments.first();
+
+        if (image) {
+            try {
+                const worker = await createWorker();
+                await worker.loadLanguage('eng');
+                await worker.initialize('eng');
+                const {
+                    data: { text },
+                } = await worker.recognize(image.url);
+                await worker.terminate();
+
+                content += text;
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        const lowerCaseContent = content.toLowerCase();
 
         // basic filter
-        if (!content.includes('earn') || !content.includes('crypto market')) {
+        if (!lowerCaseContent.includes('earn') || !(lowerCaseContent.includes('crypto') && lowerCaseContent.includes('market'))) {
             return;
         }
 
         if (
-            content.includes('https://') ||
-            message.content.includes('WhatsApp') ||
-            message.content.includes('Telegram') ||
-            content.includes('commission')
+            lowerCaseContent.includes('https://') ||
+            content.includes('WhatsApp') ||
+            content.includes('Telegram') ||
+            lowerCaseContent.includes('commission')
         ) {
             await message.delete().catch(console.error);
         }
