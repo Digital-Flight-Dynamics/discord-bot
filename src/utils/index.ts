3b02f1f97460c8d5f57ad1d00c74ae3502be8eca
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2024-02-09 15:26:08 -0600

    disable auto .when

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 import { ClientEvents } from 'discord.js';
 import { autoKick } from './autoKick';
-import { autoWhen } from './autoWhen';
 import { cryptoScamDelete } from './cryptoScamDelete';
 import { joinMessages, leaveMessages } from './joinLeave';
 import { memberCounter } from './memberCounter';
@@ -11,4 +10,4 @@ export interface UtilDefinition {
     execute: (...args: any[]) => void;
 }
 
-export default [addRole, autoKick, autoWhen, cryptoScamDelete, joinMessages, leaveMessages, memberCounter, removeRole];
+export default [addRole, autoKick, cryptoScamDelete, joinMessages, leaveMessages, memberCounter, removeRole];
