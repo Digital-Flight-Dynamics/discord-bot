381367e3aea56ef9b73d0d2322d691ef283494c2
Aurora Takemi <98479040+alepouna@users.noreply.github.com>
2024-05-13 16:12:31 +0300

    installer cmd

---
+++ src/commands/a350x/installer.ts
@@ -0,0 +1,16 @@
+import { CommandCategories, CommandDefinition } from '../index';
+import { createEmbed } from '../../lib/embed';
+
+export const installer: CommandDefinition = {
+    names: ['installer', 'xbox'],
+    description: "Provides information about the installer setup",
+    category: CommandCategories.A350X,
+    execute: async (message, args) => {
+        const embed = createEmbed({
+            title: 'How will the A350X be available?',
+            description: 'Unless circumstances change, we plan on utilizing the FlyByWire installer to offer the A350X. ',
+            footer: { text: 'The A350X has not released yet, therefore these plans may change prior to the initial release.' },
+        });
+        await message.channel.send({ embeds: [embed] }).catch(console.error);
+    },
+};
